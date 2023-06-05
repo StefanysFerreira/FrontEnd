2bf2fa2d1d80c95cc168c7fbb05a84a3bb038f20
HenriqueSantosHS <henriquelp321@gmail.com>
2023-06-05 14:36:23 -0300

    Melhoria da issue #3

--- our_movies/src/components/MovieCard.jsx
+++ our_movies/src/components/MovieCard.jsx
@@ -1,20 +1,35 @@
 import { Link } from "react-router-dom";
 
 import {FaThumbsDown, FaThumbsUp } from "react-icons/fa";
-import { BsHandThumbsDown, BsHandThumbsUp } from "react-icons/bs";
+// import { BsHandThumbsDown } from "react-icons/bs";
+
+import { useState } from "react";
+import "./avaliar.css"
 
 const imagesURL = import.meta.env.VITE_IMG;
 
 const MovieCard = ({ movie, showLink = true }) => {
+  const[count, setCount] = useState(0);
+  const handlebotaoPositivo = () => setCount((prevCount) => prevCount + 1);
+  const handlebotaoNegativo = () => setCount((prevCount) =>  prevCount - 1);
+
   return (
     <div className="movie-card">
       <img src={imagesURL + movie.poster_path} alt={movie.title} />
       <h2>{movie.title}</h2>
-      <p>
-        <BsHandThumbsUp />{movie.vote_average}<BsHandThumbsDown/></p>
+      <div className="avaliacao">
+        <button className="positivo" onClick={handlebotaoPositivo}><FaThumbsUp /></button>
+        <p className="contador">{count}</p>
+        <button className="negativo" onClick={handlebotaoNegativo}><FaThumbsDown /></button>
+      </div>
+
+      
       {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
     </div>
   );
+
 };
+
+
 
 export default MovieCard;
