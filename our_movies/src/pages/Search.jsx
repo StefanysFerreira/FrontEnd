cd4b4902eff1dd6097c7e243b5f41aac984c9688
HenriqueSantosHS <henriquelp321@gmail.com>
2023-06-07 15:18:37 -0300

    implementação da Issue #8

--- our_movies/src/pages/Search.jsx
+++ our_movies/src/pages/Search.jsx
@@ -6,6 +6,8 @@ const searchURL = import.meta.env.VITE_SEARCH;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 import "./MoviesGrid.css";
+import { FaSadCry, FaSadTear } from "react-icons/fa";
+import { BiSad } from "react-icons/bi";
 
 const Search = () => {
   const [searchParams] = useSearchParams();
@@ -28,6 +30,8 @@ const Search = () => {
     <div className="container">
       <h2 className="title">
         Resultados para: <span className="query-text">{query}</span>
+        {movies.length == 0 &&
+          <h1 className="filme-erro">Filme não encontrado em nossa base de dados!  <FaSadTear color="#f7d354"/></h1>}
       </h2>
       <div className="movies-container">
         {movies.length > 0 &&
