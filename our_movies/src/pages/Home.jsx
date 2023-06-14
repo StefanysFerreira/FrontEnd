9104d8d9a2c755e395cc44d7633988f495ad8012
HenriqueSantosHS <henriquelp321@gmail.com>
2023-06-14 14:20:52 -0300

    implementação da Issue #4

--- our_movies/src/pages/Home.jsx
+++ our_movies/src/pages/Home.jsx
@@ -1,18 +1,22 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
 
 import "./MoviesGrid.css";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
+const itemsPerPage = 6; // Define a quantidade de filmes por página
 
 const Home = () => {
   const [topMovies, setTopMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   const getTopRatedMovies = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
     setTopMovies(data.results);
+    setTotalPages(Math.ceil(data.results.length / itemsPerPage));
   };
 
   useEffect(() => {
@@ -21,14 +25,36 @@ const Home = () => {
     getTopRatedMovies(topRatedUrl);
   }, []);
 
-  console.log(topMovies);
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const renderMovies = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const moviesToShow = topMovies.slice(startIndex, endIndex);
+
+    return moviesToShow.map((movie) => (
+      <MovieCard key={movie.id} movie={movie} />
+    ));
+  };
 
   return (
     <div className="container">
       <h2 className="title">Melhores filmes:</h2>
       <div className="movies-container">
-        {topMovies.length > 0 &&
-          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+        {renderMovies()}
+      </div>
+      <div className="pagination">
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map((pageNumber) => (
+          <button
+            key={pageNumber}
+            className={pageNumber === currentPage ? "active" : ""}
+            onClick={() => handlePageChange(pageNumber)}
+          >
+            {pageNumber}
+          </button>
+        ))}
       </div>
     </div>
   );
--- our_movies/src/pages/Search.jsx
+++ our_movies/src/pages/Search.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
 
 const searchURL = import.meta.env.VITE_SEARCH;
 const apiKey = import.meta.env.VITE_API_KEY;
+const itemsPerPage = 6; // Define a quantidade de filmes por página
 
 import "./MoviesGrid.css";
 import { FaSadCry, FaSadTear } from "react-icons/fa";
@@ -11,14 +12,16 @@ import { BiSad } from "react-icons/bi";
 
 const Search = () => {
   const [searchParams] = useSearchParams();
-
   const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
   const query = searchParams.get("q");
 
   const getSearchedMovies = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
     setMovies(data.results);
+    setTotalPages(Math.ceil(data.results.length / itemsPerPage));
   };
 
   useEffect(() => {
@@ -26,17 +29,47 @@ const Search = () => {
     getSearchedMovies(searchWithQueryURL);
   }, [query]);
 
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const renderMovies = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const moviesToShow = movies.slice(startIndex, endIndex);
+
+    return moviesToShow.map((movie) => (
+      <MovieCard key={movie.id} movie={movie} />
+    ));
+  };
+
   return (
     <div className="container">
       <h2 className="title">
         Resultados para: <span className="query-text">{query}</span>
-        {movies.length == 0 &&
-          <h1 className="filme-erro">Filme não encontrado em nossa base de dados!  <FaSadTear color="#f7d354"/></h1>}
+        {movies.length === 0 && (
+          <h1 className="filme-erro">
+            Filme não encontrado em nossa base de dados!{" "}
+            <FaSadTear color="#f7d354" />
+          </h1>
+        )}
       </h2>
-      <div className="movies-container">
-        {movies.length > 0 &&
-          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
-      </div>
+      <div className="movies-container">{renderMovies()}</div>
+      {totalPages > 1 && (
+        <div className="pagination-container">
+          <div className="pagination">
+            {Array.from({ length: totalPages }, (_, index) => index + 1).map((pageNumber) => (
+              <button
+                key={pageNumber}
+                className={pageNumber === currentPage ? "active" : ""}
+                onClick={() => handlePageChange(pageNumber)}
+              >
+                {pageNumber}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
