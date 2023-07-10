cbde8d9530189d0823e3e52ed310797283714506
Henrique dos Santos Alves <henriquelp321@gmail.com>
2023-07-10 16:13:17 -0300

    correção de bug

--- our_movies/src/components/Layout.jsx
+++ our_movies/src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./Navbar"
-import { Footer } from "./Footer"
+import Footer from "./Footer"
 import './Layout.css'
 import './Navbar.css'
 
